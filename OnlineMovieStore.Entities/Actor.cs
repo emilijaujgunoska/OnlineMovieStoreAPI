991a25b70c7f841ac7e02db4609be27bc1f16df9
Sara Trpkovska <sara.trpkovska26@gmail.com>
2023-08-15 00:08:43 +0200

    actor and category

--- OnlineMovieStore.Entities/Actor.cs
+++ OnlineMovieStore.Entities/Actor.cs
@@ -1,12 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OnlineMovieStore.Entities
 {
-    internal class Actor
+    public class Actor
     {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(100)]
+        public string Name { get; set; } = null!;
+        [StringLength(1100)]
+        public string? ShortDescription { get; set; }
+        public bool IsPopular { get; set; }
     }
 }
