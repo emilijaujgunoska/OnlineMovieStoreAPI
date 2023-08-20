a7a15137fe340422a7b83fca155b6284481d79ad
emili <emili@DESKTOP-KQTM4N3>
2023-08-20 23:06:39 +0200

    Project reference (entities) added

--- OnlineMovieStore.Data/OnlineMovieStoreDbContext.cs
+++ OnlineMovieStore.Data/OnlineMovieStoreDbContext.cs
@@ -1,17 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Newtonsoft.Json;
-using OnlineMovieStore.Entities;
-using OnlineMovieStore.Entities.Config;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
-using System.Threading.Tasks;
-public class OnlineMovieStoreDbContext : DbContext
+﻿
+namespace OnlineMovieStore.Data
 {
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+    using OnlineMovieStore.Entities;
+   // using OnlineMovieStore.Entities.Config;
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Security.Cryptography;
+    using System.Text;
+    using System.Threading.Tasks;
+    public class OnlineMovieStoreDbContext : DbContext
+    {
 
-} 
-    
+    }
 
+}
